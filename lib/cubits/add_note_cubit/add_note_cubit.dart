423191938c2c344dc927f35cc4898675ff5067e0
abdelrahman1820 <124547898+abdelrahman1820@users.noreply.github.com>
2024-01-10 20:09:15 +0200

    registeradapter

--- lib/cubits/add_note_cubit/add_note_cubit.dart
+++ lib/cubits/add_note_cubit/add_note_cubit.dart
@@ -1,6 +1,12 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:notesapp/cubits/add_note_cubit/add_note_state.dart';
+import 'package:notesapp/models/note_model.dart';
 
-class NotesCubit extends Cubit<NotesState> {
-  NotesCubit() : super(AddNoteInitial());
+class Addnotecubit extends Cubit<NotesState> {
+  Addnotecubit() : super(AddNoteInitial());
+  addnote(NoteModel note) {
+
+
+
+  }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/adapters.dart';
 import 'package:notesapp/const.dart';
+import 'package:notesapp/models/note_model.dart';
 import 'package:notesapp/views/notes_page.dart';
 
 void main() async {
-  await Hive.initFlutter();
-  await Hive.openBox(kNotesBox);
+  await Hive.initFlutter();//بنشأ Hive
+  await Hive.openBox(kNotesBox);//بفتح بوكس عشان اخزن فيه 
+  Hive.registerAdapter(NoteModelAdapter());//بعمل ريجيستر علي الفايل الي اتعمله اوتو كرييت 
   runApp(const NotesApp());
 }
 
