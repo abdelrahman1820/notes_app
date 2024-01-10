0f49389b235511dd1e01677d2b5f4ff4965fc1f4
abdelrahman1820 <124547898+abdelrahman1820@users.noreply.github.com>
2024-01-10 19:04:45 +0200

    addnotestate

--- lib/cubits/add_note_cubit/add_note_cubit.dart
+++ lib/cubits/add_note_cubit/add_note_cubit.dart
@@ -2,5 +2,5 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:notesapp/cubits/add_note_cubit/add_note_state.dart';
 
 class NotesCubit extends Cubit<NotesState> {
-  NotesCubit() : super(NotesInitial());
+  NotesCubit() : super(AddNoteInitial());
 }
--- lib/cubits/add_note_cubit/add_note_state.dart
+++ lib/cubits/add_note_cubit/add_note_state.dart
@@ -1,4 +1,14 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 abstract class NotesState {}
-class NotesInitial extends NotesState{}
+
+class AddNoteInitial extends NotesState {}
+
+class AddNoteLoading extends NotesState {}
+
+class AddNoteSuccess extends NotesState {}
+
+class AddnoteFailur extends NotesState {
+  final String errMessgae;
+  AddnoteFailur({required this.errMessgae});
+}
